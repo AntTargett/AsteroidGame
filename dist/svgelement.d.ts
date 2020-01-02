925786fd2f9f6a779ef4249712e3c4ea0ad7162e
Antony Targett <atargett@au1.ibm.com>
2020-01-02 15:12:36 +1100

    Removed cached dist

--- dist/svgelement.d.ts
+++
@@ -1,9 +0,0 @@
-import Observable from "./observable";
-declare class Elem {
-    elem: Element;
-    constructor(svg: HTMLElement, tag: string, parent?: Element);
-    attr(name: string): string;
-    attr(name: string, value: string | number): this;
-    observe<T extends Event>(event: string): Observable<T>;
-}
-export default Elem;
